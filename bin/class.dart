0f9cf796383deddd9d1554e98f4bd3126d5b9ed6
Sultan Room <sultanroomcode@gmail.com>
2025-01-12 19:17:30 +0700

    Update Person attributes and add comments for clarity

--- bin/class.dart
+++ bin/class.dart
@@ -16,9 +16,13 @@ void main() {
   print(person.name);
   print(person.address);
   print(person.age);
+  // https://youtu.be/k0ycD2aqPzU?t=1087
+  person.name = 'Satria';
   person.age = 30;
+  // person.address = 'Indonesia'; //bakal error karena final
   print(person.age);
 
+// https://youtu.be/k0ycD2aqPzU?t=1048
   Person person2 = Person();
   print(person2);
 }
