6f5174f2144c4b5a854c61ddbef1cac54df6b8d0
Sultan Room <sultanroomcode@gmail.com>
2025-01-12 19:11:05 +0700

    firt init

---
+++ bin/class.dart
@@ -0,0 +1,19 @@
+class Person {
+  // https://youtu.be/k0ycD2aqPzU?t=900
+  String name = "Guest";
+  int? age; //null safety
+  final String address = "123 Main St"; //final variable tidak bisa diubah lagi
+
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+void main() {
+  var person = Person();
+  print(person);
+
+  Person person2 = Person();
+  print(person2);
+}
